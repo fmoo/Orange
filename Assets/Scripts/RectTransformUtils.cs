0284d70925949121f5d4442732875b79e6770001
Peter Ruibal <ruibalp@gmail.com>
2020-02-01 11:37:58 -0800

    Pivot Points and other rect transform helpers

--- Assets/Scripts/RectTransformUtils.cs
+++ Assets/Scripts/RectTransformUtils.cs
@@ -10,6 +10,36 @@ public static class RectTransformUtils {
 		public Vector3 LowerRight;
 	}
 
+	public class PivotPoints {
+		public Vector2 UpperLeft;
+		public Vector2 TopCenter;
+		public Vector2 UpperRight;
+		public Vector2 MiddleLeft;
+		public Vector2 CenterMiddle;
+		public Vector2 MiddleRight;
+		public Vector2 LowerLeft;
+		public Vector2 CenterBottom;
+		public Vector2 LowerRight;
+	}
+
+	public static readonly PivotPoints Pivots = new PivotPoints() {
+		UpperLeft = new Vector2(0.0f, 1.0f),
+		TopCenter = new Vector2(0.5f, 1.0f),
+		UpperRight = new Vector2(1.0f,1.0f),
+		MiddleLeft = new Vector2(0.0f, 0.5f),
+		CenterMiddle = new Vector2(0.5f, 0.5f),
+		MiddleRight = new Vector2(1.0f, 0.5f),
+		LowerLeft = new Vector2(0.0f, 0.0f),
+		CenterBottom = new Vector2(0.5f, 0.0f),
+		LowerRight = new Vector2(1.0f, 0.0f),
+	};
+
+	public static void ExpandToFillParent(this RectTransform rt) {
+		rt.anchorMin = Vector2.zero;
+		rt.anchorMax = Vector2.one;
+		rt.sizeDelta = Vector2.one;
+	}
+
 	public static RectCorners GetLocalCornersStruct(this RectTransform rt) {
 		Vector3[] fourCornersArray = new Vector3[4];
 		rt.GetLocalCorners(fourCornersArray);
@@ -20,6 +50,21 @@ public static class RectTransformUtils {
 			UpperRight = fourCornersArray[2],
 			LowerRight = fourCornersArray[3],
 		};
+	}
+
+	public static Vector3 CenterMiddle(this RectTransform rt) {
+		var cs = rt.GetLocalCornersStruct();
+		return Vector3.Lerp(cs.UpperLeft, cs.LowerRight, 0.5f);
+	}
+
+	public static Vector3 CenterBottom(this RectTransform rt) {
+		var cs = rt.GetLocalCornersStruct();
+		return Vector3.Lerp(cs.LowerLeft, cs.LowerRight, 0.5f);
+	}
+
+	public static Vector3 CenterTop(this RectTransform rt) {
+		var cs = rt.GetLocalCornersStruct();
+		return Vector3.Lerp(cs.UpperLeft, cs.UpperRight, 0.5f);
 	}
 
 	public static Vector3 MiddleLeft(this RectTransform rt) {
