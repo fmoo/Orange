63ea08653d473b5e37f4fa98627c77f1f996fe3f
Peter Ruibal <ruibalp@gmail.com>
2020-02-01 11:38:23 -0800

    Fix formatting

--- Assets/Scripts/ThirdParty/CoroutineHelper/MonoBehaviorSingleton.cs
+++ Assets/Scripts/ThirdParty/CoroutineHelper/MonoBehaviorSingleton.cs
@@ -1,22 +1,25 @@
 using UnityEngine;
 
-public class MonoBehaviourSingleton< TSelfType > : MonoBehaviour where TSelfType : MonoBehaviour
+public class MonoBehaviourSingleton<TSelfType> : MonoBehaviour where TSelfType : MonoBehaviour
 {
-	private static TSelfType m_Instance = null;
-	public static TSelfType Instance
-	{
-		get
-		{
-			if (m_Instance == null)
-			{
-				m_Instance = (TSelfType)FindObjectOfType(typeof(TSelfType));
-				if (m_Instance == null)
-				{
-					m_Instance = (new GameObject(typeof(TSelfType).Name)).AddComponent<TSelfType>();
-				}
-				DontDestroyOnLoad(m_Instance.gameObject);
-			}
-			return m_Instance;
-		}
-	}
+    private static TSelfType m_Instance = null;
+    public static TSelfType Instance
+    {
+        get
+        {
+            if (m_Instance == null)
+            {
+                m_Instance = (TSelfType)FindObjectOfType(typeof(TSelfType));
+                if (m_Instance == null)
+                {
+                    m_Instance = (new GameObject(typeof(TSelfType).Name)).AddComponent<TSelfType>();
+                }
+                if (!Application.isEditor)
+                {
+                    DontDestroyOnLoad(m_Instance.gameObject);
+                }
+            }
+            return m_Instance;
+        }
+    }
 }
