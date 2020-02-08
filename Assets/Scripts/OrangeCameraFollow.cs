e521c5c26859d4b04aab8495182a8fa813e06d12
Peter Ruibal <ruibalp@gmail.com>
2020-02-07 20:22:27 -0800

    Make CameraFollow work for sprite
    And fix bug by using LateUpdate

--- Assets/Scripts/OrangeCameraFollow.cs
+++ Assets/Scripts/OrangeCameraFollow.cs
@@ -2,15 +2,29 @@
 using System.Collections;
 
 public class OrangeCameraFollow : MonoBehaviour {
-	public Collider2D actor;
+	public Collider2D targetCollider;
+	public SpriteRenderer targetSprite;
 
-	void Update () {
+	Bounds GetBounds() {
+		if (targetSprite != null) {
+			return targetSprite.bounds;
+		} else if (targetCollider != null) {
+			return targetSprite.bounds;
+		} else {
+			return Camera.current.OrthographicBounds();
+		}
+	}
+
+	void LateUpdate () {
+		DoUpdateNaive();
+	}
+
+	void DoUpdateNaive() {
 		// This approach kind of sucks. Ideally we'd have some sort of
 		// bounding rectangle and if the player moves outside of it, we move
 		// to follow.
-
 		Transform t = GetComponent<Transform> ();
-		Vector3 v = actor.bounds.center;
+		Vector3 v = GetBounds().center;
 		t.position = new Vector3(v.x, v.y, t.position.z);
 	}
 }
