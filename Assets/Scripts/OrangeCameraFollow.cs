4e7d11a9705b4fa7042cf8581ee438c40ce7e252
Peter Ruibal <ruibalp@gmail.com>
2020-02-08 19:33:52 -0800

    Tweaks to CameraFollow to make it more dynamic

--- Assets/Scripts/OrangeCameraFollow.cs
+++ Assets/Scripts/OrangeCameraFollow.cs
@@ -2,29 +2,42 @@
 using System.Collections;
 
 public class OrangeCameraFollow : MonoBehaviour {
-	public Collider2D targetCollider;
-	public SpriteRenderer targetSprite;
+    public Collider2D targetCollider;
+    public SpriteRenderer targetSprite;
+    public Camera affectCamera;
 
-	Bounds GetBounds() {
-		if (targetSprite != null) {
-			return targetSprite.bounds;
-		} else if (targetCollider != null) {
-			return targetSprite.bounds;
-		} else {
-			return Camera.current.OrthographicBounds();
-		}
-	}
+    Bounds GetTargetBounds() {
+        if (targetSprite != null) {
+            return targetSprite.bounds;
+        } else if (targetCollider != null) {
+            return targetSprite.bounds;
+        } else {
+            return Camera.current.OrthographicBounds();
+        }
+    }
 
-	void LateUpdate () {
-		DoUpdateNaive();
-	}
+    void LateUpdate() {
+        DoUpdateNaive();
+    }
 
-	void DoUpdateNaive() {
-		// This approach kind of sucks. Ideally we'd have some sort of
-		// bounding rectangle and if the player moves outside of it, we move
-		// to follow.
-		Transform t = GetComponent<Transform> ();
-		Vector3 v = GetBounds().center;
-		t.position = new Vector3(v.x, v.y, t.position.z);
-	}
+
+	public float cameraSpeed = 2f;
+    void DoUpdateNaive() {
+        // This approach kind of sucks. Ideally we'd have some sort of
+        // bounding rectangle and if the player moves outside of it, we move
+        // to follow.
+        Camera c = (affectCamera ?? Camera.current);
+        Transform t = c.transform;
+        Vector3 v = GetTargetBounds().center;
+        // var cameraBounds = c.OrthographicBounds();
+		// cameraBounds.size = (cameraBounds.size * 0.7f) + (Vector3.forward * 100f);
+		// if (cameraBounds.Contains(v)) {
+		// 	return;
+		// }
+        t.position = Vector3.Lerp(
+			t.position,
+			new Vector3(v.x, v.y, t.position.z),
+			cameraSpeed * Time.deltaTime);
+        // t.position = new Vector3(v.x, v.y, t.position.z);
+    }
 }
