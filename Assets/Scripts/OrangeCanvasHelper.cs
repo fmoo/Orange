6509d47bd5b4c5f02c59a62cd5be69a7c5652e63
Peter Ruibal <ruibalp@gmail.com>
2020-02-10 17:30:15 -0800

    Canvas helper for pixel perfect canvas

---
+++ Assets/Scripts/OrangeCanvasHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class OrangeCanvasHelper : MonoBehaviour {
+    new public UnityEngine.U2D.PixelPerfectCamera camera;
+    private CanvasScaler canvasScaler;
+    private int lastHeight;
+    private int lastReferenceHeight;
+
+    void Start() {
+        DoUpdateSize();
+    }
+
+    void Awake() {
+        DoUpdateSize();
+    }
+
+    void Update() {
+        DoUpdateSize();
+    }
+
+    void DoUpdateSize() {
+        if (camera == null) return;
+        var referenceHeight = camera.refResolutionY;
+        if (Screen.height == lastHeight && referenceHeight == lastReferenceHeight) return;
+        if (referenceHeight <= 0) return;
+        Debug.Log(Screen.height + "..." + lastHeight);
+        canvasScaler = gameObject.GetOrCreateComponent<CanvasScaler>();
+        canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ConstantPixelSize;
+        canvasScaler.scaleFactor = Screen.height / referenceHeight;
+        Debug.Log("Height=" + Screen.height + "  refHeight=" + referenceHeight);
+        lastHeight = Screen.height;
+        lastReferenceHeight = referenceHeight;
+    }
+}
