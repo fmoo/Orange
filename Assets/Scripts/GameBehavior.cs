8a13ecee5f261c5490d168d6e1d6c4e49eb3e8e4
Peter Ruibal <ruibalp@gmail.com>
2020-02-11 10:43:39 -0800

    Fix bugs from rename

--- Assets/Scripts/GameBehavior.cs
+++ Assets/Scripts/GameBehavior.cs
@@ -4,11 +4,11 @@ using System.Collections;
 public class GameBehavior : MonoBehaviour {
 
 	protected T GetOrCreateComponent<T>() where T : Component  {
-		return ComponentUtils.GetOrCreateComponent<T> (this);
+		return ComponentExtensions.GetOrCreateComponent<T> (this);
 	}
 
 	protected GameObject GetOrCreateGameObject(string name) {
-		return ComponentUtils.GetOrCreateGameObject (this, name);
+		return ComponentExtensions.GetOrCreateGameObject (this, name);
 	}
 
 	protected bool IsInEditorMode() {
