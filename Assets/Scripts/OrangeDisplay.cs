4b02094d769d6099b1d9ce7b36aeb41daa79662c
Peter Ruibal <ruibalp@gmail.com>
2020-02-16 08:36:38 -0800

    Added module to monitor display resolution changes

---
+++ Assets/Scripts/OrangeDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrangeDisplay : MonoBehaviour {
+    public int Width;
+    public int Height;
+    public bool IsValid = false;
+    private int LastWidth;
+    private int LastHeight;
+
+    public System.Action OnResolutionChanged = null;
+
+    void Start() {
+        Width = Screen.width;
+        Height = Screen.height;
+        IsValid = true;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Width = Screen.width;
+        Height = Screen.height;
+        if (LastWidth != Width || LastHeight != Height) {
+            OnResolutionChanged?.Invoke();
+            LastWidth = Width;
+            LastHeight = Height;
+        }
+    }
+}
