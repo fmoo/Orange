45c32f32bba5bd1c194de647420e7393f1c5af91
Peter Ruibal <ruibalp@gmail.com>
2020-02-16 08:33:43 -0800

    Implement bounding, await for CameraFollow

--- Assets/Scripts/OrangeCameraFollow.cs
+++ Assets/Scripts/OrangeCameraFollow.cs
@@ -6,6 +6,8 @@ public class OrangeCameraFollow : MonoBehaviour {
     public SpriteRenderer targetSprite;
     public Camera affectCamera;
 
+    public Bounds? cameraBounds = null;
+
     Bounds GetTargetBounds() {
         if (targetSprite != null) {
             return targetSprite.bounds;
@@ -21,7 +23,7 @@ public class OrangeCameraFollow : MonoBehaviour {
     }
 
 
-	public float cameraSpeed = 2f;
+    public float cameraSpeed = 2f;
     void DoUpdateNaive() {
         // This approach kind of sucks. Ideally we'd have some sort of
         // bounding rectangle and if the player moves outside of it, we move
@@ -30,14 +32,31 @@ public class OrangeCameraFollow : MonoBehaviour {
         Transform t = c.transform;
         Vector3 v = GetTargetBounds().center;
         // var cameraBounds = c.OrthographicBounds();
-		// cameraBounds.size = (cameraBounds.size * 0.7f) + (Vector3.forward * 100f);
-		// if (cameraBounds.Contains(v)) {
-		// 	return;
-		// }
-        t.position = Vector3.Lerp(
-			t.position,
-			new Vector3(v.x, v.y, t.position.z),
-			cameraSpeed * Time.deltaTime);
+        // cameraBounds.size = (cameraBounds.size * 0.7f) + (Vector3.forward * 100f);
+        // if (cameraBounds.Contains(v)) {
+        // 	return;
+        // }
+
+        var newPosition = Vector3.Lerp(
+            t.position,
+            new Vector3(v.x, v.y, t.position.z),
+            cameraSpeed * Time.deltaTime);
+
+        if (cameraBounds is Bounds cb) {
+            if (cb.Contains(newPosition) != true) {
+                newPosition = cb.ClosestPoint(newPosition);
+            }
+        }
+
+        t.position = newPosition;
         // t.position = new Vector3(v.x, v.y, t.position.z);
+    }
+
+    public IEnumerator WaitForMovementDone() {
+        while (true) {
+            var dist = (transform.position.xy() - GetTargetBounds().center.xy()).sqrMagnitude;
+            if (dist < 0.01f) break;
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }
