f74af54d78b3d33e2cd1a55bbe9cee2bbe186623
Peter Ruibal <ruibalp@gmail.com>
2020-02-16 08:47:35 -0800

    Basic mouse handling for OrangeCursor
    Also Move Start() tweaks to OnValidate()

--- Assets/Scripts/OrangeCursor.cs
+++ Assets/Scripts/OrangeCursor.cs
@@ -10,21 +10,45 @@ public class OrangeCursor : MonoBehaviour {
 
     public Vector2 padding;
 
-    // Start is called before the first frame update
-    void Start() {
-        rectTransform = gameObject.GetOrCreateComponent<RectTransform>();
+    void OnValidate() {
+        var renderer = GetComponent<Image>();
+        renderer.raycastTarget = false;
+        rectTransform = this.GetOrCreateComponent<RectTransform>();
         scaleCoeff = new Vector2(1f / rectTransform.localScale.x, 1f / rectTransform.localScale.y);
     }
 
+    void Start() {
+        OnValidate();
+    }
+
     // Update is called once per frame
+    private List<RaycastResult> pointerResults = new List<RaycastResult>();
+    private Vector3 lastMousePosition = Vector3.zero;
     void Update() {
+        var mousePosition = Input.mousePosition;
+        if (mousePosition != lastMousePosition) {
+            // TODO: Can this be memoized/reused?
+            PointerEventData pointerData = new PointerEventData(EventSystem.current) {
+                pointerId = -1,
+            };
+            pointerData.position = Input.mousePosition;
+            EventSystem.current.RaycastAll(pointerData, pointerResults);
+            foreach (var pointerResult in pointerResults) {
+                var selectable = pointerResult.gameObject.GetComponent<Selectable>();
+                if (selectable != null && selectable.gameObject != EventSystem.current.currentSelectedGameObject && selectable.interactable) {
+                    selectable.Select();
+                }
+            }
+            lastMousePosition = mousePosition;
+        }
+
         var currentSelection = EventSystem.current.currentSelectedGameObject;
         if (currentSelection == null) {
             return;
         }
         // TODO: Add a flag to support *animating* movement / size to the new values
         rectTransform.position = currentSelection.transform.position;
-        rectTransform.sizeDelta = padding + currentSelection.GetComponent<RectTransform>().sizeDelta * scaleCoeff ;
+        rectTransform.sizeDelta = padding + currentSelection.GetComponent<RectTransform>().sizeDelta * scaleCoeff;
     }
 
     public void SnapToTarget(Selectable target) {
@@ -34,6 +58,6 @@ public class OrangeCursor : MonoBehaviour {
 
         var targetTransform = target.GetComponent<RectTransform>();
         rectTransform.position = targetTransform.position;
-        rectTransform.sizeDelta = padding + targetTransform.sizeDelta * scaleCoeff ;
+        rectTransform.sizeDelta = padding + targetTransform.sizeDelta * scaleCoeff;
     }
 }
