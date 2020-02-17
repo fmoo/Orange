e5f53b0358125c5919c77f288986542e11c720d2
Peter Ruibal <ruibalp@gmail.com>
2020-02-17 10:56:22 -0800

    Add a panel helper

---
+++ Assets/Scripts/OrangePanel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OrangePanel : MonoBehaviour {
+    public RectTransform rectTransform;
+    public Image backgroundImage;
+
+    public void Show() {
+        rectTransform.gameObject.SetActive(true);
+    }
+    public void ShowWithColor(Color color) {
+        Show();
+        if (backgroundImage != null)
+            backgroundImage.color = color;
+    }
+
+    public void Hide() {
+        rectTransform.gameObject.SetActive(false);
+    }
+
+    void OnValidate() {
+        if (rectTransform == null)
+            rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Start() {
+        OnValidate();
+    }
+
+    void Update() {
+
+    }
+}
