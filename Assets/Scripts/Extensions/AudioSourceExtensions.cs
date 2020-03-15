7ac122177978ca5160fe0c795a43b7506809ae83
Peter Ruibal <ruibalp@gmail.com>
2020-03-14 17:52:38 -0700

    AudioSource.WaitForAudioFade()
    Add AudioSourceExtensions that include an implementation of
    a "WaitForAudioFade" which implements an IEnumerator that "fades" an
    AudioSource from it's current value to to the target volume across a
    target duration.

---
+++ Assets/Scripts/Extensions/AudioSourceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AudioSourceExtensions {
+
+    public static IEnumerator WaitForAudioFade(this AudioSource audioSource, float targetVolume, float duration) {
+        if (audioSource == null) yield break;
+        float timeElapsed = 0f;
+        float startVolume = audioSource.volume;
+
+        while (timeElapsed < duration) {
+            yield return null;
+            timeElapsed += Time.deltaTime;
+            var nextVolume = Mathf.Lerp(startVolume, targetVolume, timeElapsed / duration);
+            audioSource.volume = nextVolume;
+        }
+        audioSource.volume = targetVolume;
+    }
+
+}
