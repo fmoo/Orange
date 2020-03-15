3c48733c1df3f7a6f47818551a7ebc89e01dcf09
Peter Ruibal <ruibalp@gmail.com>
2020-03-14 17:50:12 -0700

    RectTransformExtensions rewrite (GetWorldCenter())
    RectTransformExtensions had some junk in it that imho didn't work quite
    right, so I'm nuking it.  After making tweaks to cursor, this method
    (GetWorldCenter()) has been the only useful thing.

--- Assets/Scripts/Extensions/RectTransformExtensions.cs
+++ Assets/Scripts/Extensions/RectTransformExtensions.cs
@@ -10,46 +10,16 @@ public static class RectTransformExtensions {
         }
     }
 
-    public static void ExpandToFillParent(this RectTransform rt) {
-        rt.anchorMin = Vector2.zero;
-        rt.anchorMax = Vector2.one;
-        rt.sizeDelta = Vector2.one;
+    public static Vector3[] GetWorldCorners(this RectTransform rect) {
+        Vector3[] corners = new Vector3[4];
+        rect.GetWorldCorners(corners);
+        return corners;
     }
 
-    public static (Vector3 LowerLeft, Vector3 UpperLeft, Vector3 UpperRight, Vector3 LowerRight) GetLocalCornersStruct(this RectTransform rt) {
-        Vector3[] fourCornersArray = new Vector3[4];
-        rt.GetLocalCorners(fourCornersArray);
-
-        return (
-            LowerLeft: fourCornersArray[0],
-            UpperLeft: fourCornersArray[1],
-            UpperRight: fourCornersArray[2],
-            LowerRight: fourCornersArray[3]
-        );
+    static Vector3[] _corners = new Vector3[4];
+    public static Vector3 GetWorldCenter(this RectTransform rect) {
+        rect.GetWorldCorners(_corners);
+        return (_corners[1] + _corners[3]) / 2.0f;
     }
 
-    public static Vector3 CenterMiddle(this RectTransform rt) {
-        var cs = rt.GetLocalCornersStruct();
-        return (cs.UpperLeft + cs.LowerRight) / 2f;
-    }
-
-    public static Vector3 CenterBottom(this RectTransform rt) {
-        var cs = rt.GetLocalCornersStruct();
-        return (cs.LowerLeft + cs.LowerRight) / 2f;
-    }
-
-    public static Vector3 CenterTop(this RectTransform rt) {
-        var cs = rt.GetLocalCornersStruct();
-        return (cs.UpperLeft + cs.UpperRight) / 2f;
-    }
-
-    public static Vector3 MiddleLeft(this RectTransform rt) {
-        var cs = rt.GetLocalCornersStruct();
-        return (cs.UpperLeft + cs.LowerLeft) / 2f;
-    }
-
-    public static Vector3 MiddleRight(this RectTransform rt) {
-        var cs = rt.GetLocalCornersStruct();
-        return (cs.UpperRight + cs.LowerRight) / 2f;
-    }
 }
