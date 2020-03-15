46dcb82f899827f1ec2833a8867670d5c1c8449e
Peter Ruibal <ruibalp@gmail.com>
2020-03-14 17:48:48 -0700

    OrangeUIImage/OrangeSpriteRenderer
    Helpers for autoconfiguring UI.Image and SpriteRender with an asset
    from an OrangeSpriteDB, without an animation.

---
+++ Assets/Scripts/Sprite/OrangeSpriteRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class OrangeSpriteRenderer : MonoBehaviour {
+    public OrangeSpriteDB sprites;
+    public SpriteRenderer spriteRender;
+
+    public string spriteName;
+
+    void OnValidate() {
+        if (spriteRender == null) spriteRender = GetComponent<SpriteRenderer>();
+        if (spriteRender != null && sprites != null && spriteName != "") {
+            var sprite = sprites.GetSprite(spriteName);
+            if (sprite != null) sprite.SetRendererSprite(spriteRender);
+        }
+    }
+
+    void Start() {
+        OnValidate();
+    }
+}
---
+++ Assets/Scripts/Sprite/OrangeUIImage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class OrangeUIImage : MonoBehaviour {
+    public OrangeSpriteDB sprites;
+    public UnityEngine.UI.Image spriteImage;
+
+    [NaughtyAttributes.Dropdown("GetEditorDropdown")]
+    [SerializeField] private string spriteName;
+
+    public void SetSpriteName(string spriteName) {
+        this.spriteName = spriteName;
+        OnValidate();
+    }
+
+    protected void OnValidate() {
+        if (spriteImage == null) spriteImage = GetComponent<UnityEngine.UI.Image>();
+        if (spriteImage != null && sprites != null && spriteName != "") {
+            var sprite = sprites.GetSprite(spriteName);
+            if (sprite != null) sprite.SetUIImageSprite(spriteImage);
+        }
+    }
+
+    protected void Start() {
+        OnValidate();
+    }
+
+    public NaughtyAttributes.DropdownList<string> GetEditorDropdown() {
+        return OrangeSpriteDB.GetEditorSpriteDropdown(sprites);
+    }
+}
