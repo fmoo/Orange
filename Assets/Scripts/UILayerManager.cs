8e3c3b56267030a58b3ffd96b940f174f4a0d280
Peter Ruibal <ruibalp@gmail.com>
2020-03-21 18:36:48 -0700

    UILayerManager: Rudimentary layer nav stack

--- Assets/Scripts/UILayerManager.cs
+++ Assets/Scripts/UILayerManager.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class UILayerManager : MonoBehaviour {
     public OrangeCanvasHelper canvas;
@@ -81,5 +84,37 @@ public class UILayerManager : MonoBehaviour {
     public void StartAudioFade(float targetVolume, float duration) {
         if (audioSource != null) StartCoroutine(audioSource.WaitForAudioFade(targetVolume, duration));
         if (audioLoopSource != null) StartCoroutine(audioLoopSource.WaitForAudioFade(targetVolume, duration));
+    }
+
+    public void PushLayer(UILayer layer) {
+        // Debug.Log($"PushLayer: {layer}");
+        PlayConfirmSound();
+        var ctx = new UILayerPushContext();
+        var selectedObject = EventSystem.current.currentSelectedGameObject;
+        if (selectedObject != null) ctx.selection = selectedObject.GetComponent<Selectable>();
+        ctx.visibleLayers.AddRange(GetComponentsInChildren<UILayer>().Where(l => l.shown));
+        ctx.visibleLayers.ForEach(l => l.Hide());
+        navigationStack.Push(ctx);
+        layer.Show();
+    }
+
+    public void PopLayer(bool useConfirmSound = false) {
+        if (navigationStack.IsNullOrEmpty()) return;
+        if (useConfirmSound) PlayConfirmSound(); else PlayCancelSound();
+        foreach (var layer in GetComponentsInChildren<UILayer>().Where(layer => layer.shown)) {
+            layer.Hide();
+        }
+        var popped = navigationStack.Pop();
+        // Debug.Log($"PopLayer: {popped}");
+        popped.visibleLayers.ForEach(layer => layer.Show(false));
+        if (popped.selection != null) {
+            popped.selection.Select();
+        }
+    }
+
+    private Stack<UILayerPushContext> navigationStack = new Stack<UILayerPushContext>();
+    private class UILayerPushContext {
+        public List<UILayer> visibleLayers = new List<UILayer>();
+        public Selectable selection = null;
     }
 }
