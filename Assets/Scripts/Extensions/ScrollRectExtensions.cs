d1edebe710f70f32b3ff29581cc5027840ff66fd
Peter Ruibal <ruibalp@gmail.com>
2020-03-21 18:34:57 -0700

    Extension: ScrollRect.ScrollToTop/Bottom

---
+++ Assets/Scripts/Extensions/ScrollRectExtensions.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public static class ScrollRectExtensions {
+    public static void ScrollToTop(this ScrollRect scrollRect) {
+        scrollRect.normalizedPosition = new Vector2(0, 1);
+    }
+    public static void ScrollToBottom(this ScrollRect scrollRect) {
+        scrollRect.normalizedPosition = new Vector2(0, 0);
+    }
+}
