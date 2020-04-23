1a836a4911ebfc2f807e6e7023e5e41e44bdca6a
Peter Ruibal <ruibalp@gmail.com>
2020-04-23 14:02:38 -0700

    Vector3.WithX/Y/Z to update a Vector on one axis

--- Assets/Scripts/Extensions/VectorExtensions.cs
+++ Assets/Scripts/Extensions/VectorExtensions.cs
@@ -20,6 +20,16 @@ public static class VectorExtensions {
         return Vector3.Lerp(a, b, Mathf.Sin(t * Mathf.PI));
     }
 
+    public static Vector3 WithX(this Vector3 v, float x) {
+        return new Vector3(x, v.y, v.z);
+    }
+    public static Vector3 WithY(this Vector3 v, float y) {
+        return new Vector3(v.x, y, v.z);
+    }
+    public static Vector3 WithZ(this Vector3 v, float z) {
+        return new Vector3(v.x, v.y, z);
+    }
+
     public static int ManhattanDistance(this Vector2Int a, Vector2Int b) {
         return Mathf.Abs(a.x - b.x) + Mathf.Abs(a.y - b.y);
     }
