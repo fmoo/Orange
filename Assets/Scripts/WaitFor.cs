6754c3f2b8e1f6fc674dd7fcd13f38582d98f407
Peter Ruibal <ruibalp@gmail.com>
2020-04-26 10:42:24 -0700

    NEW: WaitFor.TimeRatio(duration, action<float>)
    WaitFor.TimeRatio is an IEnumerator that you can gen across
    `duration` secionds, that will be invoked every frame
    with the current duration ratio passed to `action`

--- Assets/Scripts/WaitFor.cs
+++ Assets/Scripts/WaitFor.cs
@@ -9,7 +9,25 @@ public static class WaitFor {
     public static IEnumerator NextFrame() {
         yield return null;
     }
-    
+
+    /// <summary>Invokes `callback` every frame for `duration` seconds, passing the ratio
+    /// from 0.0 to 1.0) of the time elapsed.</summary>
+    public static IEnumerator TimeRatio(float duration, System.Action<float> callback) {
+        if (duration == 0f) {
+            callback(1.0f);
+            yield break;
+        }
+
+        float timeElapsed = 0f;
+        while (timeElapsed < duration) {
+            yield return null;
+            timeElapsed += Time.deltaTime;
+            // clamp
+            callback(timeElapsed < duration ? timeElapsed / duration : 1.0f);
+        }
+    }
+
+
     public static IEnumerator SecondsOrInput(float seconds, params InputButton[] buttons) {
         var t0 = Time.time;
         if (seconds <= 0f) yield break;
