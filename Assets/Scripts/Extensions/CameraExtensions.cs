efcddfee150dd92a835827af58cdc7bb0caf4ec1
Peter Ruibal <ruibalp@gmail.com>
2020-06-14 13:05:10 -0700

    Camera.GetBoundsRaycasted
    Similar to Camera.OrthographicBounds() but different

--- Assets/Scripts/Extensions/CameraExtensions.cs
+++ Assets/Scripts/Extensions/CameraExtensions.cs
@@ -9,4 +9,10 @@ public static class CameraExtensions {
             new Vector3(cameraHeight * screenAspect, cameraHeight, 0));
         return bounds;
     }
+
+    public static Bounds GetBoundsRaycasted(this Camera camera) {
+        var bottomLeft = camera.ScreenToWorldPoint(Vector3.zero);
+        var topRight = camera.ScreenToWorldPoint(new Vector3(camera.pixelWidth, camera.pixelHeight));
+        return new Bounds(topRight + bottomLeft / 2, topRight - bottomLeft);
+    }
 }
