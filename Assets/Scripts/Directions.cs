763d05fe5afd4485d6a9124800b1e34d8a37e9cb
Peter Ruibal <ruibalp@gmail.com>
2020-06-22 20:16:57 -0700

    new Directions4/8 compass and conversions methods
    Added a bunch of more helpers for Directions8/4
    Also fixed a bug where DOWN was being treated as right

--- Assets/Scripts/Directions.cs
+++ Assets/Scripts/Directions.cs
@@ -24,6 +24,44 @@ public enum Directions8 {
 }
 
 public static class DirectionsUtils {
+    public static Directions4 CompassToDirections4(string compass) {
+        if (compass == "N") {
+            return Directions4.UP;
+        } else if (compass == "E") {
+            return Directions4.RIGHT;
+        } else if (compass == "S") {
+            return Directions4.DOWN;
+        } else if (compass == "W") {
+            return Directions4.LEFT;
+        } else {
+            Debug.LogWarning("Invalid compass direction provided to DirectionsUtils.CompassToDirections4");
+            return Directions4.UP;
+        }
+    }
+
+    public static Directions8 CompassToDirections8(string compass) {
+        if (compass == "N") {
+            return Directions8.UP;
+        } else if (compass == "E") {
+            return Directions8.RIGHT;
+        } else if (compass == "S") {
+            return Directions8.DOWN;
+        } else if (compass == "W") {
+            return Directions8.LEFT;
+        } else if (compass == "NE") {
+            return Directions8.UPRIGHT;
+        } else if (compass == "SE") {
+            return Directions8.DOWNRIGHT;
+        } else if (compass == "SW") {
+            return Directions8.DOWNLEFT;
+        } else if (compass == "NW") {
+            return Directions8.UPLEFT;
+        } else {
+            Debug.LogWarning("Invalid compass direction provided to DirectionsUtils.CompassToDirections4");
+            return Directions8.UP;
+        }
+    }
+
     public static float ToFloat(this Directions2 d) {
         switch (d) {
             case Directions2.LEFT:
@@ -62,7 +100,7 @@ public static class DirectionsUtils {
             case Directions4.RIGHT:
                 return Vector2.right;
             case Directions4.DOWN:
-                return Vector2.right;
+                return Vector2.down;
         }
         return Vector2.zero;
     }
@@ -95,7 +133,7 @@ public static class DirectionsUtils {
             case Directions8.RIGHT:
                 return Vector2.right;
             case Directions8.DOWN:
-                return Vector2.right;
+                return Vector2.down;
             case Directions8.DOWNLEFT:
                 return Vector2.down + Vector2.left;
             case Directions8.DOWNRIGHT:
@@ -150,6 +188,36 @@ public static class DirectionsUtils {
         }
     }
 
+    public static Directions8 ToDirections8(this Directions4 d) {
+        switch (d) {
+            case Directions4.DOWN:
+                return Directions8.DOWN;
+            case Directions4.LEFT:
+                return Directions8.LEFT;
+            case Directions4.UP:
+                return Directions8.UP;
+            case Directions4.RIGHT:
+                return Directions8.RIGHT;
+            default:
+                // TODO: Log
+                return Directions8.UP;
+        }
+    }
+
+    public static string ToCompass(this Directions4 d) {
+        switch (d) {
+            case Directions4.DOWN:
+                return "S";
+            case Directions4.LEFT:
+                return "W";
+            case Directions4.UP:
+                return "N";
+            case Directions4.RIGHT:
+                return "E";
+            default:
+                return "N";
+        }
+    }
     public static string ToCompass(this Directions8 d) {
         switch (d) {
             case Directions8.DOWN:
