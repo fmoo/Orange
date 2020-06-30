1b9cb9366c33f5243bb99a8974b40add0bfc46b9
Peter Ruibal <ruibalp@gmail.com>
2020-06-30 11:13:13 -0700

    Extensions for waiting for Particle System completion

---
+++ Assets/Scripts/Extensions/ParticleExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ParticleExtensions {
+    public static bool GetIsDone(this ParticleSystem ps) {
+        if (ps == null) return true;
+
+        if (ps.isEmitting || ps.particleCount > 0) {
+            return false;
+        }
+        for (int ii = 0; ii < ps.subEmitters.subEmittersCount; ii++) {
+            if (ps.subEmitters.GetSubEmitterSystem(ii).particleCount > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static IEnumerator WaitForDone(this ParticleSystem ps) {
+        if (ps.main.loop == true) {
+            throw new UnityException("WaitForDone called on looping Particle System!");
+        }
+        while (!ps.GetIsDone()) {
+            yield return WaitFor.Seconds(0.1f);
+        }
+    }
+
+}
