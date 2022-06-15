a082cfbc67ea6558c9b2f55deaf23ff8d23aae9f
Peter Ruibal <ruibalp@gmail.com>
2022-06-14 19:10:09 -0700

    Reformat ColorUtils

--- Assets/Scripts/ColorUtils.cs
+++ Assets/Scripts/ColorUtils.cs
@@ -2,20 +2,20 @@
 using UnityEngine.UI;
 
 public static class ColorUtils {
-	public static Color WithAlpha(this Color source, float a) {
-		return new Color(source.r, source.g, source.b, a);
-	}
+    public static Color WithAlpha(this Color source, float a) {
+        return new Color(source.r, source.g, source.b, a);
+    }
 
-	public static Color FromBytes(byte r, byte g, byte b, byte a) {
-		return new Color(
-			r / 255.0f,
-			g / 255.0f,
-			b / 255.0f,
-			a / 255.0f
-		);
-	}
+    public static Color FromBytes(byte r, byte g, byte b, byte a) {
+        return new Color(
+            r / 255.0f,
+            g / 255.0f,
+            b / 255.0f,
+            a / 255.0f
+        );
+    }
 
-	public static Color FromBytes(byte r, byte g, byte b) {
-		return FromBytes(r, g, b, 255);
-	}
+    public static Color FromBytes(byte r, byte g, byte b) {
+        return FromBytes(r, g, b, 255);
+    }
 }
