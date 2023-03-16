411ea51f128639ac971363cc8321abdd730fc5d0
Peter Ruibal <ruibalp@gmail.com>
2023-03-15 21:40:18 -0700

    New Parallax implementation

--- Assets/Scripts/Tiled/OrangeTiledImporter.cs
+++ Assets/Scripts/Tiled/OrangeTiledImporter.cs
@@ -1,14 +1,14 @@
-using UnityEngine;
 using SuperTiled2Unity;
 using SuperTiled2Unity.Editor;
-using UnityEngine.Tilemaps;
 using UnityEditor;
+using UnityEngine;
+using UnityEngine.Tilemaps;
 
 [AutoCustomTmxImporter()]
 abstract public class OrangeTiledImporter : CustomTmxImporter {
 
-    virtual protected void PostProcessSuperObject(SuperObject customObject, string mapName) {}
-    virtual protected void PostProcessSuperLayer(SuperLayer layer, string mapName) {}
+    virtual protected void PostProcessSuperObject(SuperObject customObject, string mapName) { }
+    virtual protected void PostProcessSuperLayer(SuperLayer layer, string mapName) { }
 
     public override void TmxAssetImported(TmxAssetImportedArgs args) {
         foreach (var customObjectSet in args.ImportedSuperMap.GetComponentsInChildren<SuperObject>()) {
@@ -42,84 +42,28 @@ abstract public class OrangeTiledImporter : CustomTmxImporter {
     }
 
     void InitParallaxLayer(SuperLayer layer, float pixelsPerUnit) {
-        CustomProperty prop;
         var props = layer.GetComponent<SuperCustomProperties>();
-
-        if (props.TryGetCustomProperty("noParallax", out prop)) {
-            if (prop.GetValueAsBool()) {
-                return;
-            }
+        if (props == null || props.GetBool("noParallax")) {
+            return;
         }
 
-        Vector2Int screenDimension = new Vector2Int(320, 180);
+        var mapProps = layer.GetComponentInParent<SuperMap>().GetComponent<SuperCustomProperties>();
 
-        bool didAddParallaxHandler = false;
-        if (layer is SuperImageLayer) {
-            var mapProps = layer.GetComponentInParent<SuperMap>().GetComponent<SuperCustomProperties>();
+        var spriteRenderer = layer.GetComponent<SpriteRenderer>();
 
-            Vector2 autoscrollSpeed = new Vector2();
-            var spriteRenderer = layer.GetComponent<SpriteRenderer>();
-            var rawImage = layer.gameObject.AddComponent<UnityEngine.UI.RawImage>();
-            rawImage.color = spriteRenderer.color;
-            rawImage.texture = spriteRenderer.sprite.texture;
-            Rect uvRect = rawImage.uvRect;
-            // Debug.LogWarning($"rawImage uvRect original dimensions are {rawImage.uvRect.width}x{rawImage.uvRect.height}");
-            // Debug.LogWarning($"texture dimensions are {rawImage.texture.width}x{rawImage.texture.height}");
-            // Debug.LogWarning($"screen dimensions are {screenDimension.x}x{screenDimension.y}");
-            uvRect.width = 1f / ((float)rawImage.texture.width / (float)screenDimension.x);
-            uvRect.height = 1f / ((float)rawImage.texture.height / (float)screenDimension.y);
+        Vector2 autoscrollSpeed = Vector2.zero;
+        autoscrollSpeed.x = props.GetFloat("autoscrollX", 0f);
+        autoscrollSpeed.y = props.GetFloat("autoscrollY", 0f);
+        var cameraScroll = new Vector2(layer.m_ParallaxX, layer.m_ParallaxY);
 
-            if (props.TryGetCustomProperty("scaleU", out prop)) {
-                uvRect.width *= prop.GetValueAsFloat();
-            }
-            if (props.TryGetCustomProperty("scaleV", out prop)) {
-                uvRect.height *= prop.GetValueAsFloat();
-            }
-            rawImage.uvRect = uvRect;
-            // Debug.LogWarning($"final uvRect dimensions are {rawImage.uvRect.width}x{rawImage.uvRect.height}");
-
-            var canvas = layer.gameObject.AddComponent<Canvas>();
-            var backgroundUILayer = LayerMask.NameToLayer("BackgroundUI");
-            if (backgroundUILayer != -1) {
-                layer.gameObject.layer = backgroundUILayer;
-            }
-            canvas.renderMode = RenderMode.ScreenSpaceCamera;
-            canvas.sortingLayerID = spriteRenderer.sortingLayerID;
-            canvas.sortingOrder = spriteRenderer.sortingOrder;
-
-            if (props.TryGetCustomProperty("autoscrollX", out prop)) {
-                autoscrollSpeed.x = prop.GetValueAsFloat();
-            }
-            if (props.TryGetCustomProperty("autoscrollY", out prop)) {
-                autoscrollSpeed.y = prop.GetValueAsFloat();
-            }
-            var cameraScroll = new Vector2(layer.m_ParallaxX, layer.m_ParallaxY);
-
-            if (autoscrollSpeed != Vector2.zero || cameraScroll != Vector2.one) {
-                var fx = layer.gameObject.AddComponent<ParallaxFX>();
-                fx.canvas = canvas;
-                fx.image = rawImage;
-                fx.scrollSpeed = autoscrollSpeed;
-                fx.cameraScroll = new Vector2(layer.m_ParallaxX, layer.m_ParallaxY);
-                // fx.baseOffset.x = (layer.m_OffsetX / screenDimension.x) + 0.5f;
-                // fx.baseOffset.y = (layer.m_OffsetY / screenDimension.y) + 0.5f;
-                // fx.cameraScroll = new Vector2(1f - layer.m_ParallaxX, 1f - layer.m_ParallaxY);
-                fx.baseOffset = new Vector2(-layer.m_OffsetX, layer.m_OffsetY);
-                fx.pixelsPerUnit = pixelsPerUnit;
-
-                // divide fx.baseOffset by image size in pixels
-                fx.baseOffset.x /= rawImage.texture.width;
-                fx.baseOffset.y /= rawImage.texture.height;
-
-                spriteRenderer.enabled = false;
-                didAddParallaxHandler = true;
-            }
+        var parallaxEffect = layer.gameObject.AddComponent<ParallaxLayerHandler>();
+        parallaxEffect.autoScrollSpeed  = autoscrollSpeed;
+        if (layer is SuperImageLayer imageLayer) {
+            parallaxEffect.repeatsX = imageLayer.m_RepeatX;
+            parallaxEffect.repeatsY = imageLayer.m_RepeatY;
         }
-
-        if (!didAddParallaxHandler) {
-            var parallaxEffect = layer.gameObject.AddComponent<ParallaxLayerHandler>();
-            parallaxEffect.layer = layer;
-        }
+        parallaxEffect.layer = layer;
+        parallaxEffect.spriteRenderer = spriteRenderer;
     }
 
     void SetObjectLayerRenderOverrides(SuperObject customObject) {
--- Assets/Scripts/Tiled/ParallaxLayerHandler.cs
+++ Assets/Scripts/Tiled/ParallaxLayerHandler.cs
@@ -1,36 +1,151 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using SuperTiled2Unity;
-using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class ParallaxLayerHandler : MonoBehaviour {
-	public SuperLayer layer;
+    public SuperLayer layer;
+    public SpriteRenderer spriteRenderer;
+    public Vector2 autoScrollSpeed = Vector2.zero;
+    public Vector2 currentScrollOffset = Vector2.zero;
+    public bool repeatsX = false;
+    public bool repeatsY = false;
 
-	private Vector3 v3 = new Vector3();
+    private Vector3 v3 = new Vector3();
 
-	public Vector3 origOffset;
-	public bool reset;
-	// public bool enableExtraSnapping = true;
+    public Vector3 origOffset;
+    public Vector2 origSize;
+    private Vector2 halfOrigSize;
+    private Vector3 halfSize;
 
-	void Start() {
-		origOffset = transform.localPosition;
-	}
+    void Start() {
+        origOffset = transform.localPosition;
+        origSize = spriteRenderer.size;
+        halfOrigSize = origSize / 2f;
+        if (repeatsX || repeatsY) {
+            spriteRenderer.drawMode = SpriteDrawMode.Tiled;
+            spriteRenderer.tileMode = SpriteTileMode.Continuous;
 
-	void LateUpdate() {
-		if (reset) {
-			layer.transform.localPosition = origOffset;
-			return;
-		}
+            // Check the source asset texture and log an error if it's not configured to repeat.
+            if (spriteRenderer.sprite.texture.wrapMode != TextureWrapMode.Repeat) {
+                Debug.LogError($"Sprite {spriteRenderer.sprite.texture.name} is not configured to repeat. Please set wrapMode to repeat.", spriteRenderer.sprite.texture);
+            }
 
-		if (layer == null) return;
-		var camera = Camera.main ?? Camera.current;
-		if (camera == null) return;
+            // If repeatingX, double the horizontal size.  If repeatsY, double the vertical size.
+            var size = origSize;
+            var newPosition = transform.localPosition;
+            if (repeatsX) {
+                size.x *= 4f;
+            }
+            if (repeatsY) {
+                size.y *= 4f;
+            }
+            spriteRenderer.size = size;
+            halfSize = spriteRenderer.size / 2f;
+        }
+    }
 
-		v3.x = origOffset.x + (camera.transform.position.x * (1f - layer.m_ParallaxX)) + (camera.rect.x / 2f * layer.m_ParallaxX * layer.m_OffsetX / 16f);
-		v3.y = origOffset.y + (camera.transform.position.y * (1f - layer.m_ParallaxY)) + (camera.rect.y / 2f * layer.m_ParallaxY * layer.m_OffsetY / 16f);
-		v3.z = layer.transform.localPosition.z;
-		layer.transform.localPosition = v3;
-	}
+    void UpdateScrollOffset() {
+        // Automatically adjust currentScrollOffset based on autoScrollSpeed.  Snap to increments of size.
+        if (autoScrollSpeed != Vector2.zero) {
+            currentScrollOffset += autoScrollSpeed * Time.deltaTime;
 
+            // NOTE: In theory we only need to the stuff below if we're repeating on these axes
+
+            // If currentScrollOffset exceeds +/- the size of the sprite, snap to the nearest increment.
+            while (currentScrollOffset.x > halfOrigSize.x) {
+                currentScrollOffset.x -= origSize.x;
+            }
+            while (currentScrollOffset.x < -halfOrigSize.x) {
+                currentScrollOffset.x += origSize.x;
+            }
+            // Same thing but for y
+            while (currentScrollOffset.y > halfOrigSize.y) {
+                currentScrollOffset.y -= origSize.y;
+            }
+            while (currentScrollOffset.y < -halfOrigSize.y) {
+                currentScrollOffset.y += origSize.y;
+            }
+        }
+    }
+
+    void LateUpdate() {
+        if (layer == null) return;
+        var camera = Camera.main ?? Camera.current;
+        if (camera == null) return;
+
+        UpdateScrollOffset();
+
+        v3.x = origOffset.x
+            + (camera.transform.position.x * (1f - layer.m_ParallaxX))
+            + (camera.rect.x / 2f * layer.m_ParallaxX * layer.m_OffsetX / 16f)
+            + currentScrollOffset.x;
+        v3.y = origOffset.y
+            + (camera.transform.position.y * (1f - layer.m_ParallaxY))
+            + (camera.rect.y / 2f * layer.m_ParallaxY * layer.m_OffsetY / 16f)
+            + currentScrollOffset.y;
+        v3.z = layer.transform.localPosition.z;
+
+        layer.transform.position = v3;
+
+        // Now, adjust so we fit within the camera viewport.
+        // We want to adjust the layer's position so that it's center is within the viewport.
+        if (repeatsX || repeatsY) {
+            var safety = 0;
+
+            // Compare the center of the sprite to the camera's position.
+            var cameraCenter = camera.transform.position;
+            var spriteCenter = layer.transform.position + halfSize;
+            if (repeatsX) {
+                // If the sprite is to the left of the camera, move it to the right.
+                while (spriteCenter.x < cameraCenter.x - origSize.x) {
+                    // Debug.LogWarning($"[{safety}] Adjusting layer {layer.name} to the right spriteCenter={v3} cameraCenter={cameraCenter} origSize={origSize}", this);
+                    v3.x += origSize.x;
+                    spriteCenter.x += origSize.x;
+                    safety++;
+                    if (safety > 30) {
+                        Debug.LogError($"Infinite loop detected.  Please check your parallax layer configuration.");
+                        break;
+                    }
+                }
+                // If the sprite is to the right of the camera, move it to the left.
+                while (spriteCenter.x > cameraCenter.x + origSize.x) {
+                    // Debug.LogWarning($"[{safety}] Adjusting layer {layer.name} to the left spriteCenter={v3} cameraCenter={cameraCenter} origSize={origSize}", this);
+                    v3.x -= origSize.x;
+                    spriteCenter.x -= origSize.x;
+                    safety++;
+                    if (safety > 30) {
+                        Debug.LogError($"Infinite loop detected.  Please check your parallax layer configuration.");
+                        break;
+                    }
+                }
+            }
+
+            if (repeatsY) {
+                // If the sprite is below the camera, move it up.
+                while (spriteCenter.y < cameraCenter.y - origSize.y) {
+                    // Debug.LogWarning($"[{safety}] Adjusting layer {layer.name} up spriteCenter={v3} cameraCenter={cameraCenter} origSize={origSize}", this);
+                    v3.y += origSize.y;
+                    spriteCenter.y += origSize.y;
+                    safety++;
+                    if (safety > 30) {
+                        Debug.LogError($"Infinite loop detected.  Please check your parallax layer configuration.");
+                        break;
+                    }
+                }
+                // If the sprite is above the camera, move it down.
+                while (spriteCenter.y > cameraCenter.y + origSize.y) {
+                    // Debug.LogWarning($"[{safety}] Adjusting layer {layer.name} down spriteCenter={v3} cameraCenter={cameraCenter} origSize={origSize}", this);
+                    v3.y -= origSize.y;
+                    spriteCenter.y -= origSize.y;
+                    safety++;
+                    if (safety > 30) {
+                        Debug.LogError($"Infinite loop detected.  Please check your parallax layer configuration.");
+                        break;
+                    }
+                }
+            }
+        }
+
+        layer.transform.position = v3;
+    }
 }
